e83bb98e0aa5dc0e7b7ff83c65689d4b74e6b841
fanton-francesco <145339574+fanton-francesco@users.noreply.github.com>
2024-10-29 11:56:24 +0100

    aggiuti express e mongoose funzionanti

---
+++ webapp/backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const mongoose = require("mongoose");
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const kittySchema = new mongoose.Schema({
+  name: String,
+});
+
+const Kitten = mongoose.model("Kitten", kittySchema);
+
+const silence = new Kitten({ name: "Silence" });
+console.log(silence.name); // 'Silence'
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});

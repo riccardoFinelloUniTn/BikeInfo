3a686ef6fe9afa9c11562e0e8c4b9d320bbd7607
MatteoBallardin <145289620+MatteoBallardin@users.noreply.github.com>
2024-11-06 17:28:48 +0100

    mezza validazione del token, sono stanco boss questo linguaggio fa schifo

--- webapp/backend/model/authentication.ts
+++ webapp/backend/model/authentication.ts
@@ -1,11 +1,11 @@
-import express, {Express} from "express";
+import express, {Express, NextFunction , Request , Response} from "express";
 import userModel from "./user.model";
 
 //TODO password criptate con hash e salt con pbkdf2
 const app :Express = express();
 const jwt  = require("jsonwebtoken");
 
-app.post('/api/v1/authentications', async function(req, res) {
+app.post('/api/v1/authentications', async function(req :Request , res : Response) {
 	
 	// find the user
 	let user = await userModel.findOne({ email: req.body.email }).exec();
@@ -24,7 +24,7 @@ app.post('/api/v1/authentications', async function(req, res) {
 	// if user is found and password is right create a token
 	var token = jwt.sign({ email: user.email }, process.env.SUPER_SECRET, { expiresIn: 86400 });
 
-	res.json({
+	res.status(200).json({
 		success: true,
 		message: 'Enjoy your token!',
 		token: token
@@ -32,3 +32,38 @@ app.post('/api/v1/authentications', async function(req, res) {
 
 });
  
+
+const tokenChecker = function(req : Request, res :Response, next : NextFunction) {
+	if(!req ){
+        return;
+    }
+   
+	// check header or url parameters or post parameters for token
+	var token =  req.headers["authorization"];
+
+	// if there is no token
+	if (!token) {
+		return res.status(401).send({ 
+			success: false,
+			message: 'No token provided.'
+		});
+	}
+
+	// decode token, verifies secret and checks exp
+	jwt.verify(token, process.env.SUPER_SECRET, function(err : Error, decoded : string) {			
+		if (err) {
+			return res.status(403).send({
+				success: false,
+				message: 'Failed to authenticate token.'
+			});		
+		} else {
+			// if everything is good, save to request for use in other routes
+            req
+			req.loggedUser = decoded;
+			next();
+		}
+	});
+	
+};
+
+module.exports = tokenChecker

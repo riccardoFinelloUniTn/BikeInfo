2376af02fb1e20dd632704781365e6e8b70a00a8
fanton-francesco <145339574+fanton-francesco@users.noreply.github.com>
2024-11-15 21:47:08 +0100

    changed navbar and added custom logo

--- webapp/frontend/src/components/ApiTest.vue
+++ webapp/frontend/src/components/ApiTest.vue
@@ -8,8 +8,7 @@
                     <span class="text-sm text-gray-500 dark:text-neutral-400">https://</span>
                 </span>
                 <input type="text" 
-                    :value="urlAPI"
-                    @input="(e: Event) => urlAPI = (e.target as HTMLInputElement)?.value || ''"
+                    v-model="urlAPI"
                     id="hs-trailing-button-add-on-with-leading-and-trailing" name="hs-trailing-button-add-on-with-leading-and-trailing" class="py-3 px-4 block w-full border-gray-200 shadow-sm rounded-0 text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">
                 <button type="button" class="w-[2.875rem] h-[2.875rem] shrink-0 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
                     @click.prevent="testApi"
